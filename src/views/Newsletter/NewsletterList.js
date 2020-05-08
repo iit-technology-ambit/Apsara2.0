59582c4dd8538fceeaaef94fff98838ac6d9d11f
rakaar <rka87338@gmail.com>
2020-05-08 19:47:45 +0530

    Added feature to sort the newslletters

--- src/views/Newsletter/NewsletterList.js
+++ src/views/Newsletter/NewsletterList.js
@@ -1,32 +1,66 @@
-import React from 'react'
-import './NewsletterList.scss'
-import newsletters from './NewslettersContent';
-
+import React, { useState, useEffect } from "react";
+import "./NewsletterList.scss";
+import newsletters from "./NewslettersContent";
+import Button from "../../components/CustomButtons/Button";
 export default function NewsletterList(props) {
+  const [gDepArr, setGdepArr] = useState([]);
+  const [latestArr, setLatestArr] = useState([]);
+  const [sorBtnTitle, setSorBtnTitle] = useState("sort by latest");
+  useEffect(() => {
+    const gDepArr = newsletters.slice(0, 5);
+    setGdepArr(gDepArr);
+
+    const latestArr = newsletters.slice(5);
+    setLatestArr(latestArr);
+  }, []);
+
+  function handleClick(href) {
+    props.history.push(`/newsletter/${href}`);
+  }
+
+  function sortGDList() {
+    // reverse the list
+    const gDepArrReverse = gDepArr.reverse();
+    setGdepArr(gDepArrReverse);
     
-    function handleClick(href) {
-        props.history.push(`/newsletter/${href}`)    
-    }
+    // change the button content
+    if (sorBtnTitle === "sort by latest") 
+      setSorBtnTitle("sort by oldest");
+     else if (sorBtnTitle === "sort by oldest") 
+      setSorBtnTitle("sort by latest");
+    
+  }
 
-    return(
-        <div>
-            <div className="newsletter-list">
-                <h1>Great Depression Again ?</h1>
-                {newsletters.map(item => {
-                    return(
-                    <div className="indiv-card" onClick={() => handleClick(item.href)}>
-                       
-                       <div className="title-date"> 
-                           <h3 className='title'>{item.title}</h3>
-                            <h3 className='date'>{item.date}</h3>
-                        </div>
-                        
-                    </div>
-                    )
-                })}
-              
-
+  return (
+    <div>
+      <div className="newsletter-list">
+        <h1>Catch the Latest !</h1>
+        {latestArr.map((item) => {
+          return (
+            <div className="indiv-card" onClick={() => handleClick(item.href)}>
+              <div className="title-date">
+                <h3 className="title">{item.title}</h3>
+                <h3 className="date">{item.date}</h3>
+              </div>
             </div>
-        </div>
-    )
+          );
+        })}
+        <br />
+        <h1>Great Depression Again ?</h1>
+        <Button color="info" round onClick={() => sortGDList()}>
+          {sorBtnTitle}
+        </Button>
+        {gDepArr.map((item) => {
+          return (
+            <div className="indiv-card" onClick={() => handleClick(item.href)}>
+              <div className="title-date">
+                <h3 className="title">{item.title}</h3>
+                <h3 className="date">{item.date}</h3>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/views/Newsletter/NewslettersContent.js
+++ src/views/Newsletter/NewslettersContent.js
@@ -1183,7 +1183,257 @@ const newsletters = [
       'href': '5',
       'next_href': '-1',
       'prev_href':'4'
-    }
+    },
+    {
+      'title': `Facebook & Jio will own India's internet🤑`,
+      'date': '8th May,2020',
+      'content':`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+          <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+            <!--[if !mso]><!-->
+            <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+            <!--<![endif]-->
+            <!--[if (gte mso 9)|(IE)]>
+            <xml>
+              <o:OfficeDocumentSettings>
+                <o:AllowPNG/>
+                <o:PixelsPerInch>96</o:PixelsPerInch>
+              </o:OfficeDocumentSettings>
+            </xml>
+            <![endif]-->
+            <!--[if (gte mso 9)|(IE)]>
+        <style type="text/css">
+          body {width: 600px;margin: 0 auto;}
+          table {border-collapse: collapse;}
+          table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+          img {-ms-interpolation-mode: bicubic;}
+        </style>
+      <![endif]-->
+            <style type="text/css">
+          body, p, div {
+            font-family: tahoma,geneva,sans-serif;
+            font-size: 14px;
+          }
+          body {
+            color: #000000;
+          }
+          body a {
+            color: #9fb61f;
+            text-decoration: none;
+          }
+          p { margin: 0; padding: 0; }
+          table.wrapper {
+            width:100% !important;
+            table-layout: fixed;
+            -webkit-font-smoothing: antialiased;
+            -webkit-text-size-adjust: 100%;
+            -moz-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+          }
+          img.max-width {
+            max-width: 100% !important;
+          }
+          .column.of-2 {
+            width: 50%;
+          }
+          .column.of-3 {
+            width: 33.333%;
+          }
+          .column.of-4 {
+            width: 25%;
+          }
+          @media screen and (max-width:480px) {
+            .preheader .rightColumnContent,
+            .footer .rightColumnContent {
+              text-align: left !important;
+            }
+            .preheader .rightColumnContent div,
+            .preheader .rightColumnContent span,
+            .footer .rightColumnContent div,
+            .footer .rightColumnContent span {
+              text-align: left !important;
+            }
+            .preheader .rightColumnContent,
+            .preheader .leftColumnContent {
+              font-size: 80% !important;
+              padding: 5px 0;
+            }
+            table.wrapper-mobile {
+              width: 100% !important;
+              table-layout: fixed;
+            }
+            img.max-width {
+              height: auto !important;
+              max-width: 100% !important;
+            }
+            a.bulletproof-button {
+              display: block !important;
+              width: auto !important;
+              font-size: 80%;
+              padding-left: 0 !important;
+              padding-right: 0 !important;
+            }
+            .columns {
+              width: 100% !important;
+            }
+            .column {
+              display: block !important;
+              width: 100% !important;
+              padding-left: 0 !important;
+              padding-right: 0 !important;
+              margin-left: 0 !important;
+              margin-right: 0 !important;
+            }
+            .social-icon-column {
+              display: inline-block !important;
+            }
+          }
+        </style>
+            <!--user entered Head Start--><!--End Head user entered-->
+          </head>
+          <body>
+            <center class="wrapper" data-link-color="#9fb61f" data-body-style="font-size:14px; font-family:tahoma,geneva,sans-serif; color:#000000; background-color:#FFFFFF;">
+              <div class="webkit">
+                <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+                  <tr>
+                    <td valign="top" bgcolor="#FFFFFF" width="100%">
+                      <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                        <tr>
+                          <td width="100%">
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                              <tr>
+                                <td>
+                                  <!--[if mso]>
+          <center>
+          <table><tr><td width="600">
+        <![endif]-->
+                                          <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                            <tr>
+                                              <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+          <tr>
+            <td role="module-content">
+              <p></p>
+            </td>
+          </tr>
+        </table><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="895bc87a-ed52-4cbf-a5f1-f5fcf79d67fc">
+          <tbody>
+            <tr>
+              <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+                <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="600" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/e195b559ad6af22e/293da5b9-baa4-49ab-a329-da9a5a9f21cd/1108x642.png">
+              </td>
+            </tr>
+          </tbody>
+        </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="894503ba-3a7a-4e3a-baf5-86fa917c24dd" data-mc-module-version="2019-10-22">
+          <tbody>
+            <tr>
+              <td style="padding:6px 0px 18px 0px; line-height:22px; text-align:inherit; background-color:#9fb61f;" height="100%" valign="top" bgcolor="#9fb61f" role="module-content"><div><div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 11px">6th May 2020</span></div><div></div></div></td>
+            </tr>
+          </tbody>
+        </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="462edd20-9ad3-406b-ac89-8ac6b3b10d9c" data-mc-module-version="2019-10-22">
+          <tbody>
+            <tr>
+              <td style="padding:18px 5px 18px 5px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif"><strong>Good evening. </strong></span><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Utkarsh here.&nbsp;</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">With so much about Jio in the news lately, I believe it makes sense for us to quickly take a look at it.&nbsp;</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif"><strong>Jio's background:</strong></span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">After being publicly launched in India in September 2016, Jio has become the largest Mobile Operator in the country and the </span>third-largest<span style="font-size: 14px; font-family: arial, helvetica, sans-serif"> in the </span>world w<span style="font-size: 14px; font-family: arial, helvetica, sans-serif">ithin 4 years. The stats are even more </span>awe-inspiring<span style="font-size: 14px; font-family: arial, helvetica, sans-serif">: ~ 400M out of 450M Indian internet MAUs (Monthly Active </span>Users<span style="font-size: 14px; font-family: arial, helvetica, sans-serif">) use Jio.</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Jio transformed Indian internet connectivity by providing extremely low cost and fast internet connection across the nation. But have you wondered how Jio could suddenly offer such low costs while the other telecom operators were charging much higher? Two reasons: Better tech relying on unused frequency spectrums and secondly, heavy subscriptions (i.e cash burn) made possible by the parent company's big balance sheet and further bolstered by heavy debt financings.</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Heavy debt financings - something to be worried about right. What to do now?&nbsp;</span></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Enter Facebook.&nbsp;</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif"><strong>The deal with Facebook:</strong></span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">FB bought 9.9% of Jio's stake at $5.7B. This went on to value Jio at $60B+! This makes Jio the 4th largest company in India before even being listed.&nbsp;</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">But why did Facebook do this deal? Also, what does Jio get out of it?</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Okay, so first let's look at what Jio gets out of this: Two things - first reduce debt, and second build a </span>super-app<span style="font-size: 14px; font-family: arial, helvetica, sans-serif">. This investment is basically an equity financing and the money raised from the deal will be used by Reliance to reduce its debts and deleverage. Reliance as a holding company has $20B+ debts and the $5.7B will wipe a big chunk off it. In parallel to this, they are hiving off their Oil business and diluting equity worth $7B to Saudi Aramco to deleverage further.</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit">The second<span style="font-size: 14px; font-family: arial, helvetica, sans-serif"> plan - build a super-app. And the super-app is where it gets interesting. Alibaba in China owns a super-app that enables you to pay like Paytm, buy like Flipkart, play games, chat, etc. And Jio desires to do the same. Even Grab in Singapore &amp; Go-Jek in Indonesia are doing the same. This is where Whatsapp will help Jio. JioMart was launched on Whatsapp a week after the FB deal and very soon all your grocery/everyday shopping will happen on Whatsapp through JioMart.</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif"><strong>What's in it for Facebook?</strong></span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Facebook tried its best to launch </span>its<span style="font-size: 14px; font-family: arial, helvetica, sans-serif"> payments provider (think Paytm, GPay) in </span>India<span style="font-size: 14px; font-family: arial, helvetica, sans-serif"> for a long time. Whatsapp Pay was prevented from launching by NPCI (basically the Government), over and over again. Libra saw no fruit either. But JioPay is allowed. Now just imagine JioPay integrated on Whatsapp. 80% of Indian internet users use Whatsapp. Won't everyone start paying from Whatsapp</span>?<span style="font-size: 14px; font-family: arial, helvetica, sans-serif"> Buy stuff on Whatsapp and then pay using the same. There goes your super-app. Jointly launched by FB and Jio.&nbsp;</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">The plan is clear for Jio (&amp; FB) - own the Indian internet and every internet business.</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Other interesting stories in tomorrow's newsletter.</span></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Till then,</span></div>
+      <div style="font-family: inherit; text-align: inherit"><span style="font-size: 14px; font-family: arial, helvetica, sans-serif">Utkarsh</span></div><div></div></div></td>
+            </tr>
+          </tbody>
+        </table><table class="module" role="module" data-type="divider" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="769b0b0c-183a-48e1-b967-3cbf3ca5d203.1.1.1">
+          <tbody>
+            <tr>
+              <td style="padding:7px 0px 10px 0px;" role="module-content" height="100%" valign="top" bgcolor="">
+                <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" height="2px" style="line-height:2px; font-size:2px;">
+                  <tbody>
+                    <tr>
+                      <td style="padding:0px 0px 2px 0px;" bgcolor="#A5A5A5"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="6e21386a-3fe1-4608-9c37-9bf808bcfa7c" data-mc-module-version="2019-10-22">
+          <tbody>
+            <tr>
+              <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit"><strong>What I've been reading today:</strong></div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><strong>1. </strong>India to offer land bigger than Luxembourg to lure factories out of China. Talk about striking when the iron is hot. (<a href="https://www.bloombergquint.com/global-economics/india-offers-land-twice-luxembourg-s-size-to-firms-leaving-china">Bloomberg</a>) $</div>
+      <div style="font-family: inherit; text-align: inherit"><br></div>
+      <div style="font-family: inherit; text-align: inherit"><strong>2. </strong>WeWork &amp; Founder Adam Neumann sue Softbank over a canceled stock deal. Will the WeWork saga ever end? (<a href="https://www.bloomberg.com/news/articles/2020-05-04/wework-s-adam-neumann-sues-softbank-over-canceled-stock-deal">Bloomberg</a>)</div>
+      <div style="font-family: inherit; text-align: inherit"><br></div><div></div></div></td>
+            </tr>
+          </tbody>
+        </table><table class="module" role="module" data-type="divider" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="26cd6c00-892f-4842-a5c4-a04a71f76237">
+          <tbody>
+            <tr>
+              <td style="padding:0px 0px 0px 0px;" role="module-content" height="100%" valign="top" bgcolor="">
+                <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" height="10px" style="line-height:10px; font-size:10px;">
+                  <tbody>
+                    <tr>
+                      <td style="padding:0px 0px 10px 0px;" bgcolor="#000000"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-size:12px; line-height:20px;"><a class="Unsubscribe--unsubscribeLink" href="{{{unsubscribe}}}" target="_blank" style="">Unsubscribe</a> - <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="">Unsubscribe Preferences</a></p></div></td>
+                                            </tr>
+                                          </table>
+                                          <!--[if mso]>
+                                        </td>
+                                      </tr>
+                                    </table>
+                                  </center>
+                                  <![endif]-->
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </center>
+          </body>
+        </html>`,
+      'href': '5',
+      'next_href': '-1',
+      'prev_href':'-1'
+  }
 ]
 
 export default newsletters
